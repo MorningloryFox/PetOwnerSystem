910e419c52ff41842e3456f355425be6b3baaaeb
MorningloryFox <morningloryfox@gmail.com>
2025-08-15 00:22:42 -0300

    vgy8

--- server/routes.ts
+++ server/routes.ts
@@ -68,7 +68,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
             companyId: user.companyId,
             company: {
               id: user.companyId,
-              name: 'Gloss Pet', // Could fetch from companies table
+              name: user.company?.name || 'Gloss Pet',
             },
           };
           
